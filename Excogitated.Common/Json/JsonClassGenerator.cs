9b47ab30717eb071b087dce5ed1d11564049a3b4
keith eskildsen <keithj17@gmail.com>
2020-09-03 03:36:14 -0500

    advanced class generation

--- Excogitated.Common/Json/JsonClassGenerator.cs
+++ Excogitated.Common/Json/JsonClassGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -7,17 +9,20 @@ using System.Threading.Tasks;
 
 namespace Excogitated.Common
 {
-    public class JsonClassGenerator
+    public class JsonClassGeneratorSettings
     {
         public enum Visibility { Public, Private, Internal, Protected }
-
-        private readonly StringBuilder _builder = new StringBuilder();
-        private int _depth = 0;
-
-        public Visibility ClassVisibility { get; set; } = Visibility.Public;
-        public Visibility PropertyVisibility { get; set; } = Visibility.Public;
+        public Visibility ClassVisibility { get; set; }
+        public Visibility PropertyVisibility { get; set; }
         public string RootName { get; set; } = "Root";
-        public char Separator { get; set; } = ' ';
+        public char IndentChar { get; set; } = '\t';
+    }
+
+    public class JsonClassGenerator
+    {
+        private readonly Dictionary<string, GeneratedClass> _generatedClasses = new Dictionary<string, GeneratedClass>();
+
+        public JsonClassGeneratorSettings Settings { get; set; }
 
         public string FromString(string json)
         {
@@ -46,18 +51,20 @@ namespace Excogitated.Common
 
         public string FromDocument(JsonDocument doc)
         {
-            _builder.Clear();
-            FromElement(doc.NotNull(nameof(doc)).RootElement, RootName);
-            return _builder.ToString();
+            var generated = FromElement(doc.NotNull(nameof(doc)).RootElement, Settings.RootName);
+            return generated.ToString(Settings);
         }
 
-        private string FromElement(JsonElement element, string name)
+        private GeneratedClass FromElement(JsonElement element, string name)
         {
             if (element.ValueKind == JsonValueKind.Object)
                 return FromObject(element, name);
             if (element.ValueKind == JsonValueKind.Array)
                 return FromArray(element, name);
-            return GetTypeName(element);
+            var typeName = GetTypeName(element);
+            var generated = new GeneratedClass { ClassName = typeName };
+            generated.ExampleValues.Add(element.ToString());
+            return generated;
         }
 
         private string GetTypeName(JsonElement element)
@@ -77,49 +84,141 @@ namespace Excogitated.Common
             return "string";
         }
 
-        private string FromObject(JsonElement element, string className)
+        private GeneratedClass FromObject(JsonElement element, string propertyName)
         {
-            className = className.NotNullOrWhitespace(nameof(className)).Trim();
-            if (char.IsLower(className[0]))
-            {
-                var first = char.ToUpper(className[0]);
-                if (className.Length > 1)
-                    className = first + className.Substring(1);
-            }
-            className += className.EndsWith("Data") ? "Info" : "Data";
-
-            _builder.Append(Separator, _depth)
-                .Append(ClassVisibility.ToString().ToLower()).Append(" class ")
-                .Append(className).AppendLine(" {");
-            _depth++;
+            var className = GetClassName(propertyName);
+            var generated = new GeneratedClass { ClassName = className };
             using var objs = element.EnumerateObject();
             while (objs.MoveNext())
             {
                 var name = objs.Current.Name;
-                var typeName = FromElement(objs.Current.Value, name);
-                if (typeName is string)
+                var nested = FromElement(objs.Current.Value, name);
+                var key = nested.ToString(Settings, 0, true);
+                if (_generatedClasses.TryGetValue(key, out var n))
+                    nested = n;
+                else
                 {
-                    _builder.Append(Separator, _depth)
-                        .Append(PropertyVisibility.ToString().ToLower()).Append(' ')
-                        .Append(typeName).Append(' ')
-                        .Append(name).Append(" { get; set; } ");
-                    var kind = objs.Current.Value.ValueKind;
-                    if (!kind.EqualsAny(JsonValueKind.Object, JsonValueKind.Array))
-                        _builder.Append("// ").Append(objs.Current.Value.GetRawText());
-                    _builder.AppendLine();
+                    while (_generatedClasses.ContainsKey(nested.ClassName + nested.ClassId))
+                        nested.ClassId++;
+                    if (nested.ClassId > 0)
+                        nested.ClassName += nested.ClassId;
+                    _generatedClasses.Add(nested.ClassName, nested);
+                    _generatedClasses.Add(key, nested);
                 }
+                generated.Properties.Add(name, nested);
             }
-            _depth--;
-            _builder.Append(Separator, _depth).Append('}').AppendLine();
-            return className;
+            return generated;
         }
 
-        private string FromArray(JsonElement element, string name)
+        private GeneratedClass FromArray(JsonElement element, string propertyName)
         {
             using var objs = element.EnumerateArray();
-            if (objs.MoveNext())
-                return FromElement(objs.Current, name) + "[]";
-            return null;
+            var className = propertyName;
+            if (className.EndsWith("es"))
+                className = className.Substring(0, className.Length - 2);
+            else if (className.EndsWith("s"))
+                className = className.Substring(0, className.Length - 1);
+            className = GetClassName(className);
+            var generated = new GeneratedClass { ClassName = className, IsArray = true };
+            while (objs.MoveNext())
+            {
+                var nested = FromElement(objs.Current, propertyName); // + "[]";
+                foreach (var p in nested.Properties)
+                    if (!generated.Properties.ContainsKey(p.Key))
+                        generated.Properties.Add(p.Key, p.Value);
+            }
+            return generated;
+        }
+
+        private static string GetClassName(string propertyName)
+        {
+            var className = string.Join(string.Empty, propertyName.Split('_')
+                .Select(s => $"{s.Substring(0, 1).ToUpper()}{s.Substring(1, s.Length - 1)}"));
+            className += className.EndsWith("Data") ? "Info" : "Data";
+            if (!className[0].IsLetter())
+            {
+                var names = className.TakeWhile(c => !c.IsLetter()).Select(c => c switch
+                {
+                    '0' => "Zero",
+                    '1' => "One",
+                    '2' => "Two",
+                    '3' => "Three",
+                    '4' => "Four",
+                    '5' => "Five",
+                    '6' => "Six",
+                    '7' => "Seven",
+                    '8' => "Eight",
+                    '9' => "Nine",
+                    '_' => "Underscore",
+                    '-' => "Dash",
+                    _ => "Unknown",
+                }).ToList();
+                names.Add(className.Substring(names.Count, className.Length - names.Count));
+                className = string.Join("_", names);
+            }
+            return className;
+        }
+    }
+
+    internal class GeneratedClass
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+        public Dictionary<string, GeneratedClass> Properties { get; } = new Dictionary<string, GeneratedClass>();
+        public List<string> ExampleValues { get; } = new List<string>();
+        public bool IsArray { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode() => ClassName?.GetHashCode() ?? 0;
+
+        public override string ToString()
+        {
+            return ToString(new JsonClassGeneratorSettings());
+        }
+
+        public string ToString(JsonClassGeneratorSettings settings, int depth = 0, bool asKey = false)
+        {
+            var builder = new StringBuilder();
+            if (!asKey)
+                builder.Append(settings.IndentChar, depth)
+                    .Append($"{settings.ClassVisibility.ToString().ToLower()} class {ClassName}")
+                    .AppendLine();
+            builder.Append(settings.IndentChar, depth).Append('{')
+                .AppendLine();
+            depth++;
+            foreach (var p in Properties)
+            {
+                builder.Append(settings.IndentChar, depth)
+                    .Append($"{settings.PropertyVisibility.ToString().ToLower()} {p.Value.ClassName}");
+                if (p.Value.IsArray)
+                    builder.Append("[]");
+                builder.Append($" {p.Key} {{ get; set; }}").AppendLine();
+            }
+            if (depth == 1)
+            {
+                foreach (var p in GetAllClasses().Distinct())
+                    if (p.ExampleValues.Count == 0)
+                    {
+                        builder.AppendLine().Append(p.ToString(settings, depth, asKey));
+                    }
+            }
+            depth--;
+            builder.Append(settings.IndentChar, depth).Append('}').AppendLine();
+            return builder.ToString();
+        }
+
+        private IEnumerable<GeneratedClass> GetAllClasses()
+        {
+            foreach (var p in Properties)
+            {
+                yield return p.Value;
+                foreach (var n in p.Value.GetAllClasses())
+                    yield return n;
+            }
         }
     }
 }
